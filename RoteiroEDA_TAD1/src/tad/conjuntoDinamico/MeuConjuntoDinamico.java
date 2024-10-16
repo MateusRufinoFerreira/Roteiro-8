fa03f639d2bae2013b8a713490f909dc9ca581ec
Mateus Rufino Ferreira <163481727+MateusRufinoFerreira@users.noreply.github.com>
2024-10-15 22:54:40 -0300

    Update MeuConjuntoDinamico.java

--- RoteiroEDA_TAD1/src/tad/conjuntoDinamico/MeuConjuntoDinamico.java
+++ RoteiroEDA_TAD1/src/tad/conjuntoDinamico/MeuConjuntoDinamico.java
@@ -4,7 +4,7 @@ public class MeuConjuntoDinamico implements ConjuntoDinamicoIF<Integer> {
 
 	private Integer[] meusDados = new Integer[10];
 	private int posInsercao = 0;
-
+	
 	@Override
 	public void inserir(Integer item) {
 		if (arrayEstaCheio()) {
@@ -13,103 +13,137 @@ public class MeuConjuntoDinamico implements ConjuntoDinamicoIF<Integer> {
 		meusDados[posInsercao] = item;
 		posInsercao++;
 	}
-
 	private boolean arrayEstaCheio() {
-		return posInsercao == meusDados.length;
-	}
+        return this.posInsercao == this.meusDados.length;
+    }
 
 	private void aumentarArray() {
-		Integer[] arrayMaior = new Integer[meusDados.length * 2];
-		System.arraycopy(meusDados, 0, arrayMaior, 0, meusDados.length);
-		meusDados = arrayMaior;
+		Integer[] copia = new Integer[this.meusDados.length * 2];
+		for(int index = 0; index<this.meusDados.length; index++){
+			copia[index] = this.meusDados[index];
+		}
+
+		this.meusDados = copia;
 	}
 
 	@Override
-	public Integer remover(Integer item) throws ConjuntoVazioException {
-		if (tamanho() == 0) {
-			throw new ConjuntoVazioException("Conjunto vazio");
-		}
-
-		for (int i = 0; i < posInsercao; i++) {
-			if (meusDados[i].equals(item)) {
-				Integer removido = meusDados[i];
-				// Deslocar elementos à esquerda
-				for (int j = i; j < posInsercao - 1; j++) {
-					meusDados[j] = meusDados[j + 1];
-				}
-				meusDados[--posInsercao] = null;
-				return removido;
-			}
-		}
-		return null;
-	}
-
-	public Integer predecessor(Integer item) {
-
-		Integer pred = null;
-		for (int i = 0; i < posInsercao; i++) {
-			if (meusDados[i] < item) {
-				if (pred == null || meusDados[i] > pred) {
-					pred = meusDados[i];
+	public Integer remover(Integer item) throws ConjuntoVazioException{
+		Integer retorno = null;
+		if(this.tamanho() == 0){
+			throw new ConjuntoVazioException();
+		} else {
+			for (int index = 0; index < this.posInsercao; index++) {
+				if (this.meusDados[index].equals(item)) {
+					for (int iterador = index; iterador < this.posInsercao - 1; iterador++) {
+						this.meusDados[iterador] = this.meusDados[iterador + 1];
+					}
+					this.meusDados[this.posInsercao - 1] = null;
+					this.posInsercao--;
+					retorno = item;
 				}
 			}
 		}
-		return pred;
+		return retorno;
 	}
 
-	public Integer sucessor(Integer item) throws Exception {
-		if (tamanho() == 0) {
-			throw new Exception("Conjunto vazio");
-		}
-		for (int i = 0; i < posInsercao - 1; i++) {
-			if (meusDados[i] != null && meusDados[i].equals(item)) {
-				return meusDados[i + 1];
+	@Override
+	public Integer predecessor(Integer item) throws ConjuntoVazioException {
+		Integer retorno = null;
+		if (this.tamanho() == 0) {
+			throw new ConjuntoVazioException();
+		} else {
+			for (int index = 0; index < this.posInsercao; index++) {
+				if (this.meusDados[index].equals(item)) {
+					if (index > 0){
+						retorno = this.meusDados[index-1];
+					}
+					break;
+				}
+				if (index==this.posInsercao-1 && retorno==null){
+					throw new ConjuntoVazioException();
+				}
 			}
 		}
+		return retorno;
+    }
 
-		return null;
+	@Override
+	public Integer sucessor(Integer item) throws ConjuntoVazioException {
+		Integer retorno = null;
+		if (this.tamanho() == 0) {
+			throw new ConjuntoVazioException();
+		} else {
+			for (int index = 0; index < this.posInsercao; index++) {
+				if (this.meusDados[index].equals(item)) {
+					if (index < this.posInsercao-1){
+						retorno = this.meusDados[index+1];
+					}
+					break;
+				}
+				if (index==this.posInsercao-1 && retorno==null){
+					throw new ConjuntoVazioException();
+				}
+			}
+		}
+		return retorno;
 	}
 
-
-
-
+	@Override
 	public int tamanho() {
-		return posInsercao;
+		int cont = 0;
+		if(this.meusDados != null){
+			for(Integer dado : this.meusDados){
+				if(dado != null){
+					cont++;
+				}
+			}
+		}
+		return cont;
 	}
 
+	@Override
 	public Integer buscar(Integer item) {
-		for (int i = 0; i < posInsercao; i++) {
-			if (meusDados[i].equals(item)) {
-				return meusDados[i];
+		Integer retorno = null;
+		for(int index = 0; index<this.posInsercao; index ++){
+			if(this.meusDados[index].equals(item)){
+				retorno = item;
 			}
 		}
-		return null;
+		return retorno;
 	}
 
+	@Override
 	public Integer minimum() throws ConjuntoVazioException {
-		if (tamanho() == 0) {
-			throw new ConjuntoVazioException("Conjunto vazio"); 		}
-
-		Integer min = meusDados[0];
-		for (int i = 1; i < posInsercao; i++) {
-			if (meusDados[i] < min) {
-				min = meusDados[i];
+		Integer retorno = null;
+		if (this.tamanho() == 0){
+			throw new ConjuntoVazioException();
+		} else {
+			Integer min = this.meusDados[0];
+			for (int index = 1; index < this.posInsercao; index++) {
+				if (this.meusDados[index] < min) {
+					min = this.meusDados[index];
+				}
 			}
+			retorno = min;
 		}
-		return min;
+        return retorno;
 	}
 
+	@Override
 	public Integer maximum() throws ConjuntoVazioException {
-		if (tamanho() == 0) {
-			throw new ConjuntoVazioException("Conjunto vazio");
-		}
-
-		Integer max = meusDados[0];
-		for (int i = 1; i < posInsercao; i++) {
-			if (meusDados[i] > max) {
-				max = meusDados[i];
+		Integer retorno = null;
+		if (this.tamanho() == 0){
+			throw new ConjuntoVazioException();
+		} else {
+			Integer max = this.meusDados[0];
+			for (int index = 1; index < this.posInsercao; index++) {
+				if (this.meusDados[index] > max) {
+					max = this.meusDados[index];
+				}
 			}
+			retorno = max;
 		}
-		return max;
+		return retorno;
 	}
+
 }
